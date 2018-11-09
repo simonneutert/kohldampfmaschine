dc63642cb9194a00bc6e4f7fec6c46db91937a68
Simon <simon.neutert@gmail.com>
2018-11-09 23:21:02 +0100

    functions speak louder than words :rage4:

--- docs/assets/js/edeka24-affiliate.js
+++ docs/assets/js/edeka24-affiliate.js
@@ -26,52 +26,75 @@ document.addEventListener("DOMContentLoaded", function(event) {
     xhr.send();
   }
 
+  function score(data, element, match) {
+    if (match.index === 0) {
+      data[element].score += 100;
+    } else if (match.index > 0) {
+      data[element].score += 10;
+    }
+  }
+
+  function filterSortMatches(data) {
+    let matches = [];
+    for (hit in data) {
+      if (data[hit].score > 100) {
+        matches.push(data[hit]);
+      }
+    }
+    matches.sort(function(a, b) {
+      return a.score - b.score;
+    });
+    return matches;
+  }
+
+  function addAttributeWithDefaultVal(data, attr, val) {
+    for (var element in data) {
+      data[element][attr] = val;
+    }
+  }
+
+  function writeLinkToHtml(data, e) {
+    const tempHTML = e.innerHTML;
+    e.innerHTML = "<a href=\"http://edeka24.de" + filterSortMatches(data).pop().url + "\" target=\"_blank\">" + tempHTML + "</a>";
+  }
+
+  function productNameList(p) {
+    return p.toLowerCase().replace(/[^A-Za-z0-9!?]/g, "_").split('_')
+  }
+
   loadJSON('/products.json',
     function(data) {
       const edeka24 = document.getElementsByTagName("e24");
       if (edeka24.length > 0) {
-        let matches = [];
-        for (var element in data) {
-          data[element].score = 0;
-        }
+        addAttributeWithDefaultVal(data, 'score', 0)
         for (e of edeka24) {
-          const eSignature = e.textContent.toLowerCase().replace(/[^A-Za-z0-9!?]/g, "_").split('_');
-          for (word of eSignature) {
+          for (word of productNameList(e.textContent)) {
             if (word.length < 4) {
-              continue;
-            }
-            for (element in data) {
-              for (productName of data[element].name.split(' ')) {
-                if (productName.length < 4) {
-                  continue;
-                }
-                let match = word.toLowerCase().match(productName.toLowerCase());
-                if (!match) {
-                  continue;
-                }
-                if (match.index === 0) {
-                  data[element].score += 100;
-                } else if (match.index > 0) {
-                  data[element].score += 10;
+              continue; // guard clause
+            } else {
+              for (element in data) {
+                for (productName of data[element].name.split(' ')) {
+                  if (productName.length < 4) {
+                    continue; // guard clause
+                  } else {
+                    let match = word.toLowerCase().match(productName.toLowerCase());
+                    if (!match) {
+                      continue; // guard clause
+                    } else {
+                      score(data, element, match)
+                    }
+                  }
                 }
               }
             }
           }
-          for (hit in data) {
-            if (data[hit].score > 100) {
-              matches.push(data[hit]);
-            }
-          }
-          matches.sort(function(a, b) {
-            return a.score - b.score;
-          });
-          let tempHTML = e.innerHTML;
-          e.innerHTML = "<a href=\"http://edeka24.de" + matches.pop().url + "/" target=\"_blank\">" + tempHTML + "</a>";
+          writeLinkToHtml(data, e)
           resetScores(data);
         } // end of edeka24 tag loop
       }
     },
     function(xhr) {
+      // ajax failed
       console.error(xhr);
     }
   );
